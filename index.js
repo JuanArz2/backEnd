73667100a5769ea941f820b6a2fc386bef1a7dcf
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-05-23 17:40:44 -0500

    Add script Nodemon-Dev

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const express = require("express");
 const servidor = express();
 
 servidor.get("/", (solud, rta) => {
+  // Solicitud Verbo HTTP: get. "/": raíz
   rta.json({
-    saludo: "Hi!",
+    saludo: "Hi, my friend! How r u?",
   });
 });
 
@@ -15,5 +16,5 @@ servidor.get("/xx", (solud, rta) => {
   rta.redirect(301, "https://www.jw.org/es/");
 });
 
-servidor.listen(3000);
+servidor.listen(3000); //3000, servidor
 console.log("Servidor puerto 3000");
